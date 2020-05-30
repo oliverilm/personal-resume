1fb3a0c6c159d036764562c95f87173012454080
oliverilm <oliverilmjarv@gmail.com>
2020-05-30 10:07:52 +0300

    .master Adjust styles for existing elements.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,14 @@
 import React from "react"
 import details from "../PersonalDetails.json"
-import CrossfadeImage from "react-crossfade-image";
 import "./styles.css"
 
 const customStyles = {
   image: {
-    width: "20vw",
-    height:"20vw",
+    width: "21vw",
+    height:"21vw",
     paddingTop: "20px",
     paddingBottom: "20px",
-    marginTop: "10px",
-    marginLeft: "10%",
+    marginLeft: "5vh",
     borderRadius: "50%",
   }
 }
@@ -24,24 +22,10 @@ const customStyles = {
 const Header = () => {
   const { images, main } = details
   const [ imageArray ] = React.useState(Object.values(images))
-  let [ imageIndex, setImageIndex ] = React.useState(0)
-
-  React.useEffect(() =>{
-    setInterval(() => {
-      setImageIndex(imageIndex === 1 ? 0 : 1)
-    }, 10000)
-  })
 
   return (
     <div className={"root"}>
-      <div>
-        <CrossfadeImage
-          style={customStyles.image}
-          src={imageArray[imageIndex]}
-          duration={1000}
-          timingFunction={"ease-out"}
-        />
-      </div>
+       <img src={imageArray[0]} alt={"not found"} style={customStyles.image}/>
       <div>
         <h1 className={"headerInformation"}>{main.firstMame} {main.lastName}</h1>
       </div>
